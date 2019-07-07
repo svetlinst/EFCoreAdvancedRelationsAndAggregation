f6940f0517926bb0b2d41b07d6fb2381db86f46b
Svetlin Stoyanov <svetlinst@gmail.com>
2019-07-07 16:21:13 +0300

    Add project files.

---
+++ BillsPaymentSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BillsPaymentSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
